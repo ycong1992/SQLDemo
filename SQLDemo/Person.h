bc3770e1cb4250e1054eb7f4aaa9e8ade4b062a5
xieyuecong <709932492@qq.com>
2020-11-05 14:19:35 +0800

    first commit

---
+++ SQLDemo/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  SQLDemo
+//
+//  Created by SZOeasy on 2020/11/5.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SQLDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  SQLDemo
+//
+//  Created by SZOeasy on 2020/11/5.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
