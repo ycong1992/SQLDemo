c28c7d8b7a3fac41508fecc80b8fd378de9c301b
xieyuecong <709932492@qq.com>
2021-03-01 17:37:39 +0800

    add

--- SQLDemo/FMDBBase.h
+++ SQLDemo/FMDBBase.h
@@ -4,6 +4,7 @@
  1）语法更加面向对象
  2）相对于CoreData框架，更加轻量级和灵活
  3）提供了多线程安全的数据库操作方法，可以有效地防止数据混乱
+ 缺点：只能在iOS平台上使用；需要写SQL语句
  主要类：
  FMDatabase：一个FMDatabase对象就代表一个单独的SQLite数据库，用来执行SQL语句
  FMResultSet：使用FMDatabase执行查询后的结果集
